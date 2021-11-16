15770fd8bbd886c594838349e42ea0e99adc6011
ragavachand <39850536+ragavachand@users.noreply.github.com>
2021-11-16 21:21:43 +0530

    Update configure_vault.sh

--- configure_vault.sh
+++ configure_vault.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Enable and configure Kubernetes Authentication
 vault auth enable kubernetes
 
 kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,component=server" -o jsonpath="{.items[0].metadata.name}") -c vault -- \
@@ -10,7 +9,6 @@ kubectl exec $(kubectl get pods --selector "app.kubernetes.io/instance=vault,com
        kubernetes_host=https://${KUBERNETES_PORT_443_TCP_ADDR}:443 \
        kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
 
-# Enable and configure PostgresSQL Dynamic secrets
 vault secrets enable database
 
 vault write database/config/wizard \
@@ -21,10 +19,8 @@ vault write database/config/wizard \
     username="postgres" \
     password="password"
 
-# Rotate the database root password
 vault write --force database/rotate-root/wizard
 
-# Create a role allowing credentials to be created with access for all tables in the DB
 vault write database/roles/db-app \
     db_name=wizard \
     creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; \
@@ -33,10 +29,8 @@ vault write database/roles/db-app \
     default_ttl="1h" \
     max_ttl="24h"
 
-# Write the policy to allow read access to the role
 vault policy write example-policy ./example-policy.hcl
 
-# Assign the policy to users who authenticate with Kubernetes service accounts called web
 vault write auth/kubernetes/role/web \
     bound_service_account_names=web \
     bound_service_account_namespaces=default \
